b34621b4853f7d9e11742eb42abc1dcc8e16668c
Lennart <lennart2265@gmail.com>
2025-04-11 11:44:24 +0200

    1.3

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import airbnb from "eslint-config-airbnb";
+import prettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,23 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      ...airbnb.rules,
+      "react/react-in-jsx-scope": "off",
+      "import/extensions": ["error", "never"],
+      "import/prefer-default-export": "off", 
+      "react/jsx-props-no-spreading": "off",
+      "react/require-default-props": "off"
+    }
+  },
+  ...compat.extends("next/core-web-vitals"),
+  prettier,
 ];
 
 export default eslintConfig;
