2350df5b06473285607bcf348451b7209184e464
Lennart <lennart2265@gmail.com>
2025-04-23 12:58:51 +0200

    fix double sidebar

--- my-app/src/app/components/ClientLayoutWrapper.tsx
+++ my-app/src/app/components/ClientLayoutWrapper.tsx
@@ -8,10 +8,7 @@ const queryClient = new QueryClient();
 export default function ClientLayoutWrapper({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <header className="p-4 border-b flex justify-between items-center bg-white shadow-sm">
-        <h1 className="text-xl font-bold text-gray-800">Athlete Performance Tracker</h1>
-        <YearSelector />
-      </header>
+      
       {children}
     </QueryClientProvider>
   );
